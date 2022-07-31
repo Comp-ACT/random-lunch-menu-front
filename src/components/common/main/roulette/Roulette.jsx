b515001a8fc926e7e0bf341b95b2ef4b5249d964
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2022-07-31 11:29:17 +0900

    Add roulette wire frame

---
+++ src/components/common/main/roulette/Roulette.jsx
@@ -0,0 +1,14 @@
+function Roulette() {
+  return (
+    <div style={{ height: '60%' }}>
+      <div style={{ width: 400, height: 400, background: 'black' }}>
+        Roulette
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center', marginTop: 100 }}>
+        <button>submit</button>
+      </div>
+    </div>
+  );
+}
+
+export default Roulette;
