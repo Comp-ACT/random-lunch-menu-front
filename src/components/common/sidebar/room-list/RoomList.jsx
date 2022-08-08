f7353032036bc190f16443cacccff1757bd58665
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2022-08-08 20:04:37 +0900

    Add scroll

--- src/components/common/sidebar/room-list/RoomList.jsx
+++ src/components/common/sidebar/room-list/RoomList.jsx
@@ -1,17 +1,23 @@
 import Room from './Room';
 
 function RoomList() {
-  const tempList = ['Comp-ACT 점심 메뉴', 'Comp-ACT 저녁 메뉴', '전능 점심 메뉴'];
+  const tempList = ['Comp-ACT 점심 메뉴', 'Comp-ACT 저녁 메뉴', '전능 점심 메뉴', 'Comp-ACT 점심 메뉴', 'Comp-ACT 저녁 메뉴', '전능 점심 메뉴', 'Comp-ACT 점심 메뉴', 'Comp-ACT 저녁 메뉴', '전능 점심 메뉴', 'Comp-ACT 점심 메뉴', 'Comp-ACT 저녁 메뉴', '전능 점심 메뉴', 'Comp-ACT 점심 메뉴', 'Comp-ACT 저녁 메뉴', '전능 점심 메뉴'];
   const tempHtml = tempList.map((temp) => (
     <Room name={temp} />
   ));
   return (
-    <div>
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      height: '60%',
+    }}
+    >
       <p>방 목록</p>
       <button>방 만들기</button>
       <button>방 참가하기</button>
       <div style={{
-        height: '60%',
+        overflow: 'scroll',
+        height: '80%',
         display: 'flex',
         flexDirection: 'column',
       }}
