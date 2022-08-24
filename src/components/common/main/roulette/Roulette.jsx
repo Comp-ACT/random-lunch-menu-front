0508761e7725ee4595fe9d9dd4a2505c8ca266cd
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2022-08-24 23:41:59 +0900

    Migrate Roulette component inline style to emotion

--- src/components/common/main/roulette/Roulette.jsx
+++ src/components/common/main/roulette/Roulette.jsx
@@ -1,10 +1,12 @@
+import { css } from '@emotion/react';
+
 function Roulette() {
   return (
-    <div style={{ height: '60%' }}>
-      <div style={{ width: 400, height: 400, background: 'black' }}>
+    <div css={css({ height: '60%' })}>
+      <div css={css({ width: 400, height: 400, background: 'black' })}>
         Roulette
       </div>
-      <div style={{ display: 'flex', justifyContent: 'center', marginTop: 100 }}>
+      <div css={css({ display: 'flex', justifyContent: 'center', marginTop: 100 })}>
         <button>submit</button>
       </div>
     </div>
