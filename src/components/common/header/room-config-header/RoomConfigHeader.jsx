1a7773960802740b280f282739d3368e4ffa0bab
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2022-08-24 23:36:49 +0900

    Migrate RoomConfigHeader component inline style to emotion

--- src/components/common/header/room-config-header/RoomConfigHeader.jsx
+++ src/components/common/header/room-config-header/RoomConfigHeader.jsx
@@ -1,16 +1,17 @@
+import { css } from '@emotion/react';
 import Hamburger from '../../../../assets/images/icons/Hamburger';
 import Person from '../../../../assets/images/icons/Person';
 import HeaderText from '../header-text/HeaderText';
 
 function RoomConfigHeader() {
   return (
-    <div style={{
+    <div css={css({
       display: 'flex',
       marginRight: 5,
-    }}
+    })}
     >
       <HeaderText text="대충 방 제목" fontSize={24} fontWeight="bold" />
-      <div style={{ width: 20 }} />
+      <div css={css({ width: 20 })} />
       <Person />
       <Hamburger />
     </div>
