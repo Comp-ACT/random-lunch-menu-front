428dd0ca964f50f6cea78da170aa0789cf521da9
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2022-08-24 23:04:39 +0900

    Migrate Header component inline style to emotion

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,25 +1,27 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 import RoomConfigHeader from './room-config-header/RoomConfigHeader';
 import StyledText from '../StyledText';
 
 function Header() {
   return (
-    <div style={{
+    <div css={css({
       display: 'flex',
-    }}
+    })}
     >
       <div
-        style={{
+        css={css({
           display: 'flex',
           justifyContent: 'space-between',
           width: '80%',
-        }}
+        })}
       >
         <StyledText text="점메추" fontSize={24} fontWeight="bold" />
         <div>
           <RoomConfigHeader />
         </div>
       </div>
-      <div style={{ display: 'flex', flexDirection: 'row-reverse', width: '20%' }}>
+      <div css={css({ display: 'flex', flexDirection: 'row-reverse', width: '20%' })}>
         <StyledText text="사용자 정보" fontSize={24} fontWeight="none" />
       </div>
     </div>
