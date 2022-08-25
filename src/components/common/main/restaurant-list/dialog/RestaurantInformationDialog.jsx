17638f8815f23be8d89754704badaeaa45f0ffc8
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2022-08-26 00:42:24 +0900

    Add RestaurantInformationDialog component wire frame

--- src/components/common/main/restaurant-list/dialog/RestaurantInformationDialog.jsx
+++ src/components/common/main/restaurant-list/dialog/RestaurantInformationDialog.jsx
@@ -1,9 +1,42 @@
-function RestaurantInformationDialog() {
+import * as PropTypes from 'prop-types';
+import { css } from '@emotion/react';
+
+function RestaurantInformationDialog({ restaurant }) {
   return (
-    <div>
-      대충 가게 이름하고 투표 정보들
+    <div css={css({
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'space-evenly',
+      height: '100%',
+    })}
+    >
+      <div>
+        {restaurant.restaurantName}
+      </div>
+      <div>
+        찬성 개수:
+        {' '}
+        {restaurant.agreeCount}
+      </div>
+      <div>
+        반대 개수:
+        {' '}
+        {restaurant.disagreeCount}
+      </div>
+      <div>
+        찬성 반대 버튼
+      </div>
     </div>
   );
 }
+
+RestaurantInformationDialog.propTypes = {
+  restaurant: PropTypes.objectOf({
+    id: PropTypes.string,
+    restaurantName: PropTypes.string,
+    agreeCount: PropTypes.number,
+    disagreeCount: PropTypes.number,
+  }).isRequired,
+};
 
 export default RestaurantInformationDialog;
