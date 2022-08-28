1f954f5db31170c87ded10394e73e8bfb0837281
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2022-08-28 13:58:29 +0900

    Update RoomAdditionDialog.jsx

--- src/components/common/sidebar/room-list/dialog/RoomAdditionDialog.jsx
+++ src/components/common/sidebar/room-list/dialog/RoomAdditionDialog.jsx
@@ -1,6 +1,15 @@
 import { css } from '@emotion/react';
+import { useEffect, useState } from 'react';
+import { isNullOrWhiteSpace } from '../../../../../utils';
 
 function RoomAdditionDialog() {
+  const [name, setName] = useState('');
+  const [isNameNullOrWhiteSpace, setIsNameNullOrWhiteSpace] = useState(false);
+  const [isNameOver15Letters, setIsNameOver15Letters] = useState(false);
+  useEffect(() => {
+    setIsNameNullOrWhiteSpace(isNullOrWhiteSpace(name));
+    setIsNameOver15Letters(name.length > 15);
+  }, [name]);
   return (
     <div css={css({
       display: 'flex',
@@ -9,11 +18,48 @@ function RoomAdditionDialog() {
       height: '100%',
     })}
     >
-      <div>
-        방제목 입력 부분
+      <div css={css({
+        fontSize: 32,
+      })}
+      >
+        방 만들기
       </div>
       <div>
-        버튼
+        <input
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+          placeholder="방 이름을 적어주세요."
+          css={css({
+            width: 400,
+            fontSize: 18,
+            height: 40,
+            backgroundColor: 'transparent',
+            borderRadius: 50,
+            textAlign: 'center',
+            boxSizing: 'border-box',
+            transition: '0.3s',
+            '&:focus': {
+              outline: 'none',
+            },
+          })}
+        />
+      </div>
+      <div>
+        <p css={css({
+          color: isNameNullOrWhiteSpace ? 'red' : 'green',
+        })}
+        >
+          1. 공백으로만 이루어진 이름은 사용하지 못합니다.
+        </p>
+        <p css={css({
+          color: isNameOver15Letters ? 'red' : 'green',
+        })}
+        >
+          2. 이름의 최대 길이는 15자입니다.
+        </p>
+      </div>
+      <div>
+        <button disabled={isNameNullOrWhiteSpace || isNameOver15Letters}>방 만들기</button>
       </div>
     </div>
   );
