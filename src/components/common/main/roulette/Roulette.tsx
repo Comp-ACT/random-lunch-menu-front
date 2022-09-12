de24dfecfe4962531a4cc1d32c6c58b6e82c6afc
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2022-09-12 20:19:56 +0900

    Implement roulette feature

--- src/components/common/main/roulette/Roulette.tsx
+++ src/components/common/main/roulette/Roulette.tsx
@@ -15,10 +15,45 @@ function Roulette() {
   const [rouletteRows, setRouletteRows] = useState<EmotionJSX.Element[]>([]);
   const [isRouletteSpinning, setIsRouletteSpinning] = useState<boolean>(false);
 
-  useEffect(() => {
-    setRouletteRows(
-      selectedRoom.restaurants
-        .flatMap(restaurant => {
+  const spinningRouletteRowsStyle = css({
+    '--index': -rouletteRows.length + 1,
+    textAlign: 'center',
+    fontSize: 70,
+    minHeight: 100,
+    width: 400,
+    borderStyle: 'solid',
+    overflow: 'scroll',
+    whiteSpace: 'nowrap',
+    boxSizing: 'border-box',
+    '@keyframes scroll': {
+      to: { transform: 'translateY(calc(var(--index) * 100%))' },
+    },
+    animation: 'scroll 0.5s linear 6',
+    '::-webkit-scrollbar': {
+      display: 'none',
+    },
+  });
+
+  let stoppedRouletteRowsStyle = css({
+    '--index': 0,
+    textAlign: 'center',
+    fontSize: 70,
+    minHeight: 100,
+    width: 400,
+    borderStyle: 'solid',
+    overflow: 'scroll',
+    whiteSpace: 'nowrap',
+    boxSizing: 'border-box',
+    transform: 'translateY(calc(var(--index) * 100%))',
+    '::-webkit-scrollbar': {
+      display: 'none',
+    },
+  });
+
+  function getRandomIndex(): number {
+    return -Math.floor(
+      Math.random() *
+        selectedRoom.restaurants.flatMap(restaurant => {
           let result: Array<rouletteRestaurant> = [];
           for (
             let i = 0;
@@ -35,35 +70,71 @@ function Roulette() {
             ];
           }
           return result;
-        })
-        .map(it => {
-          return (
-            <span
-              css={css({
-                '--index': -rouletteRows.length + 1,
-                textAlign: 'center',
-                fontSize: 70,
-                minHeight: 100,
-                width: 400,
-                borderStyle: 'solid',
-                overflow: 'scroll',
-                whiteSpace: 'nowrap',
-                boxSizing: 'border-box',
-                '@keyframes scroll': {
-                  to: { transform: 'translateY(calc(var(--index) * 100%))' },
-                },
-                animation: isRouletteSpinning ? 'scroll 0.5s linear 10' : '',
-                '::-webkit-scrollbar': {
-                  display: 'none',
-                },
-              })}
-            >
-              {it.restaurantName}
-            </span>
-          );
-        }),
+        }).length,
     );
+  }
+
+  function setRandomRouletteIndex() {
+    stoppedRouletteRowsStyle = css({
+      '--index': getRandomIndex(),
+      textAlign: 'center',
+      fontSize: 70,
+      minHeight: 100,
+      width: 400,
+      borderStyle: 'solid',
+      overflow: 'scroll',
+      whiteSpace: 'nowrap',
+      boxSizing: 'border-box',
+      transform: 'translateY(calc(var(--index) * 100%))',
+      '::-webkit-scrollbar': {
+        display: 'none',
+      },
+    });
+  }
+
+  function getRouletteRowsFromSelectedRoom(): EmotionJSX.Element[] {
+    return selectedRoom.restaurants
+      .flatMap(restaurant => {
+        let result: Array<rouletteRestaurant> = [];
+        for (
+          let i = 0;
+          i < restaurant.agreeCount - restaurant.disagreeCount;
+          i++
+        ) {
+          result = [
+            ...result,
+            {
+              roomId: selectedRoom.id,
+              restaurantId: restaurant.id,
+              restaurantName: restaurant.restaurantName,
+            },
+          ];
+        }
+        return result;
+      })
+      .map(it => {
+        return (
+          <span
+            css={
+              isRouletteSpinning
+                ? spinningRouletteRowsStyle
+                : stoppedRouletteRowsStyle
+            }
+          >
+            {it.restaurantName}
+          </span>
+        );
+      });
+  }
+
+  useEffect(() => {
+    setRandomRouletteIndex();
+    setRouletteRows(getRouletteRowsFromSelectedRoom());
   }, [selectedRoom, isRouletteSpinning]);
+
+  useEffect(() => {
+    setIsRouletteSpinning(false);
+  }, [selectedRoom]);
 
   return (
     <div css={css({ height: '60%' })}>
@@ -84,10 +155,15 @@ function Roulette() {
       >
         <button
           onClick={() => {
-            setIsRouletteSpinning(!isRouletteSpinning);
+            setIsRouletteSpinning(true);
+
+            setTimeout(() => {
+              setIsRouletteSpinning(false);
+            }, 3000);
           }}
+          disabled={isRouletteSpinning}
         >
-          submit
+          추첨하기
         </button>
       </div>
     </div>
