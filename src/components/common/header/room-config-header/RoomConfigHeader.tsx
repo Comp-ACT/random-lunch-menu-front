b15bf122a025312ca869daba13ebf0acac7d25ab
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2022-09-14 00:03:49 +0900

    Create header modals

--- src/components/common/header/room-config-header/RoomConfigHeader.tsx
+++ src/components/common/header/room-config-header/RoomConfigHeader.tsx
@@ -4,8 +4,13 @@ import Hamburger from '../../../../assets/images/icons/Hamburger';
 import Person from '../../../../assets/images/icons/Person';
 import HeaderText from '../header-text/HeaderText';
 import { selectedRoomSelector } from '../../../../recoil/atoms';
+import { useState } from 'react';
+import Modal from '../../dialog/Modal';
 
 function RoomConfigHeader() {
+  const [personDialogFlag, setPersonDialogFlag] = useState<boolean>(false);
+  const [hamburgerDialogFlag, setHamburgerDialogFlag] =
+    useState<boolean>(false);
   const selectedRoom = useRecoilValue(selectedRoomSelector);
   const iconSize = 48;
   return (
@@ -21,6 +26,9 @@ function RoomConfigHeader() {
         fontWeight="bold"
       />
       <div
+        onClick={() => {
+          setPersonDialogFlag(true);
+        }}
         css={css({
           cursor: 'pointer',
           marginRight: 20,
@@ -29,6 +37,9 @@ function RoomConfigHeader() {
         <Person size={iconSize} />
       </div>
       <div
+        onClick={() => {
+          setHamburgerDialogFlag(true);
+        }}
         css={css({
           cursor: 'pointer',
           marginRight: 20,
@@ -36,6 +47,24 @@ function RoomConfigHeader() {
       >
         <Hamburger size={iconSize} />
       </div>
+      {personDialogFlag && (
+        <Modal
+          closeModal={() => {
+            setPersonDialogFlag(false);
+          }}
+        >
+          <div></div>
+        </Modal>
+      )}
+      {hamburgerDialogFlag && (
+        <Modal
+          closeModal={() => {
+            setHamburgerDialogFlag(false);
+          }}
+        >
+          <div></div>
+        </Modal>
+      )}
     </div>
   );
 }
