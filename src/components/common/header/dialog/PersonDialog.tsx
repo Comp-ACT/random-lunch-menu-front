a6982189bf6054b2ca6ed3c8629be49ee6076824
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2022-09-14 21:24:14 +0900

    Update PersonDialog.tsx

--- src/components/common/header/dialog/PersonDialog.tsx
+++ src/components/common/header/dialog/PersonDialog.tsx
@@ -1,5 +1,29 @@
+import { useRecoilValue } from 'recoil';
+import { selectedRoomSelector } from '../../../../recoil/atoms';
+import { css } from '@emotion/react';
+
 function PersonDialog() {
-  return <></>;
+  const selectedRoom = useRecoilValue(selectedRoomSelector);
+  return (
+    <div
+      css={css({
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        height: '100%',
+      })}
+    >
+      <div
+        css={css({
+          fontSize: 32,
+        })}
+      >
+        {selectedRoom.roomName} 구성원
+      </div>
+      <div>대충 사람들 정보</div>
+      <div>초대 링크</div>
+    </div>
+  );
 }
 
 export default PersonDialog;
