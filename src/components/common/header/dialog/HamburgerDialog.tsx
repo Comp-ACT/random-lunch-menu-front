102d9a1b31236d00f71ec7634b43070b206da8e8
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2022-09-14 21:25:35 +0900

    Update HamburgerDialog.tsx

--- src/components/common/header/dialog/HamburgerDialog.tsx
+++ src/components/common/header/dialog/HamburgerDialog.tsx
@@ -1,5 +1,29 @@
+import { css } from '@emotion/react';
+import { useRecoilValue } from 'recoil';
+import { selectedRoomSelector } from '../../../../recoil/atoms';
+
 function HamburgerDialog() {
-  return <></>;
+  const selectedRoom = useRecoilValue(selectedRoomSelector);
+  return (
+    <div
+      css={css({
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        height: '100%',
+      })}
+    >
+      <div
+        css={css({
+          fontSize: 32,
+        })}
+      >
+        {selectedRoom.roomName} 설정
+      </div>
+      <div>방장인지 따라서 다르게 보이는 메뉴들</div>
+      <div>대충 방 나가기</div>
+    </div>
+  );
 }
 
 export default HamburgerDialog;
